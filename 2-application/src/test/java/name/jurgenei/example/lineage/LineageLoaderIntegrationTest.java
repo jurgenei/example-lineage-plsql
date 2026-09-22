9ca8ad8114b75c9047dc451f2e18d1afc89b0650
Jurgen Hildebrand <Jurgen.Hildebrand@ing.com>
2026-09-22 19:23:06 +0200

    fix LineageLoaderIntegrationTest

--- 2-application/src/test/java/name/jurgenei/example/lineage/LineageLoaderIntegrationTest.java
+++ 2-application/src/test/java/name/jurgenei/example/lineage/LineageLoaderIntegrationTest.java
@@ -16,7 +16,7 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 class LineageLoaderIntegrationTest {
 
     @Container
